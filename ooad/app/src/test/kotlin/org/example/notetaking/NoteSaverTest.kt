60d8895f03f94a9a6c4ed09b6dc600fe938a9717
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-27 10:19:49 -0300

    Implement NoteSaverTest

--- ooad/app/src/test/kotlin/org/example/notetaking/NoteSaverTest.kt
+++ ooad/app/src/test/kotlin/org/example/notetaking/NoteSaverTest.kt
@@ -4,5 +4,12 @@ import kotlin.test.Test
 
 class NoteSaverTest {
     @Test fun noteSaverWorks() {
+        val id = NoteSaver.add("Note 1")
+        println(NoteSaver.edit(id, content = "Some content"))
+        NoteSaver.save()
+        NoteSaver.delete(id)
+        println(NoteSaver.viewAll())
+        NoteSaver.sync()
+        println(NoteSaver.viewAll())
     }
 }
